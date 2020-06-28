663bab6a7d14932965e480bd213eedfcdbfe9f5f
AdhithanD <adhithanmca@gmail.com>
2020-06-28 20:11:34 +0530

    Update Solution.java
    Counting animals

--- Solution.java
+++ Solution.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 class Animal {
 	String animalName;
 
@@ -151,30 +155,82 @@ class Dolphins extends Animal {
 	Dolphins(String animalName) {
 		super(animalName);
 	}
-	
+
 	void swim() {
 		System.out.println("can swim: " + animalName);
 	}
 }
+
 class Butterfly extends Bird {
 
 	Butterfly(String animalName) {
 		super(animalName);
 	}
-	
+
 	void sing() {
 		System.out.println("I am not singing: " + animalName);
 	}
 }
+
 class Caterpillar extends Bird {
 
 	Caterpillar(String animalName) {
 		super(animalName);
 	}
-	
+
 	void fly() {
 		System.out.println("I am not flying: " + animalName);
 	}
+}
+
+class ModelAnimal {
+
+	private String animalName;
+	private boolean sing;
+	private boolean swim;
+	private boolean walk;
+	private boolean fly;
+
+	public String getAnimalName() {
+		return animalName;
+	}
+
+	public void setAnimalName(String animalName) {
+		this.animalName = animalName;
+	}
+
+	public boolean isSing() {
+		return sing;
+	}
+
+	public void setSing(boolean sing) {
+		this.sing = sing;
+	}
+
+	public boolean isSwim() {
+		return swim;
+	}
+
+	public void setSwim(boolean swim) {
+		this.swim = swim;
+	}
+
+	public boolean isWalk() {
+		return walk;
+	}
+
+	public void setWalk(boolean walk) {
+		this.walk = walk;
+	}
+
+	public boolean isFly() {
+		return fly;
+	}
+
+	public void setFly(boolean fly) {
+		this.fly = fly;
+	}
+
 }
 
 public class Solution {
@@ -212,6 +268,122 @@ public class Solution {
 		Caterpillar caterpillar = new Caterpillar("Caterpillar");
 		caterpillar.fly();
 		caterpillar.walk();
+		List<ModelAnimal> animals = setValues();
+		Long count=animals.stream().collect(Collectors.counting());
 		
+		
+
+		long countFlyAnimals = animals.stream().filter(c -> c.isFly() == true).count();
+		long countWalkAnimals = animals.stream().filter(c -> c.isWalk() == true).count();
+		long countSingAnimals = animals.stream().filter(c -> c.isSing() == true).count();
+		long countSwimAnimals = animals.stream().filter(c -> c.isSwim() == true).count();
+		
+		System.out.println("Count for Flying Animals: " + countFlyAnimals);
+		System.out.println("Count for Walking Animals: " + countWalkAnimals);
+		System.out.println("Count for Singing Animals: " + countSingAnimals);
+		System.out.println("Count for Swiming Animals: " + countSwimAnimals);
+	}
+
+	public static List<ModelAnimal> setValues() {
+		List<ModelAnimal> animals = new ArrayList<ModelAnimal>();
+
+		ModelAnimal bird = new ModelAnimal();
+		bird.setAnimalName("Bird");
+		bird.setFly(true);
+		bird.setSing(true);
+		bird.setSwim(false);
+		bird.setWalk(true);
+		animals.add(bird);
+
+		ModelAnimal duck = new ModelAnimal();
+		duck.setAnimalName("Duck");
+		duck.setFly(true);
+		duck.setSing(true);
+		duck.setSwim(true);
+		duck.setWalk(true);
+		animals.add(duck);
+
+		ModelAnimal chicken = new ModelAnimal();
+		chicken.setAnimalName("Chicken");
+		chicken.setFly(false);
+		chicken.setSing(true);
+		chicken.setSwim(false);
+		chicken.setWalk(true);
+		animals.add(chicken);
+
+		ModelAnimal rooster = new ModelAnimal();
+		rooster.setAnimalName("Rooster");
+		rooster.setFly(false);
+		rooster.setSing(true);
+		rooster.setSwim(false);
+		rooster.setWalk(true);
+		animals.add(rooster);
+
+		ModelAnimal parrot = new ModelAnimal();
+		parrot.setAnimalName("Parrot");
+		parrot.setFly(true);
+		parrot.setSing(true);
+		parrot.setSwim(false);
+		parrot.setWalk(true);
+		animals.add(parrot);
+
+		ModelAnimal fish = new ModelAnimal();
+		fish.setAnimalName("Fish");
+		fish.setFly(false);
+		fish.setSing(false);
+		fish.setSwim(true);
+		fish.setWalk(false);
+		animals.add(fish);
+
+		ModelAnimal shark = new ModelAnimal();
+		shark.setAnimalName("Shark");
+		shark.setFly(false);
+		shark.setSing(false);
+		shark.setSwim(true);
+		shark.setWalk(false);
+		animals.add(shark);
+
+		ModelAnimal clownfish = new ModelAnimal();
+		clownfish.setAnimalName("Clownfish");
+		clownfish.setFly(false);
+		clownfish.setSing(false);
+		clownfish.setSwim(true);
+		clownfish.setWalk(false);
+		animals.add(clownfish);
+
+		ModelAnimal dolhpin = new ModelAnimal();
+		dolhpin.setAnimalName("Dolhpin");
+		dolhpin.setFly(false);
+		dolhpin.setSing(false);
+		dolhpin.setSwim(true);
+		dolhpin.setWalk(false);
+		animals.add(dolhpin);
+
+		ModelAnimal dog = new ModelAnimal();
+		dog.setAnimalName("Dog");
+		dog.setFly(false);
+		dog.setSing(true);
+		dog.setSwim(false);
+		dog.setWalk(true);
+		animals.add(dog);
+
+		ModelAnimal butterfly = new ModelAnimal();
+		butterfly.setAnimalName("Butterfly");
+		butterfly.setFly(true);
+		butterfly.setSing(false);
+		butterfly.setSwim(false);
+		butterfly.setWalk(false);
+		animals.add(butterfly);
+
+		ModelAnimal cat = new ModelAnimal();
+		cat.setAnimalName("Cat");
+		cat.setFly(false);
+		cat.setSing(true);
+		cat.setSwim(false);
+		cat.setWalk(true);
+		animals.add(cat);
+
+		return animals;
+
 	}
 }
